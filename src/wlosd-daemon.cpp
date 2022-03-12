90c3edfdcf3f71fbcfce0c1151cb59762ad4c1ee
James Walmsley <jamesw@graphcore.ai>
2022-03-12 18:27:22 +0000

    [SHELL] Support gtk_layer_shell

--- src/wlosd-daemon.cpp
+++ src/wlosd-daemon.cpp
@@ -1,25 +1,57 @@
+#include "gdkmm/window.h"
 #include <gtkmm/application.h>
 #include <gtkmm/button.h>
 #include <gtkmm/window.h>
 #include <gtkmm/image.h>
+#include <gtkmm/box.h>
+#include <gtk-layer-shell/gtk-layer-shell.h>
 
-class Daemon : public Gtk::Window {
+class OsdWindow : public Gtk::Window {
     public:
-	Daemon() : m_image("images/volume_muted.png")
+	OsdWindow() : m_image("images/volume_muted.png")
 	{
+		auto screen = get_screen();
+		auto visual = screen->get_rgba_visual();
+
+		gtk_layer_init_for_window(gobj());
+		gtk_layer_set_layer(gobj(), GTK_LAYER_SHELL_LAYER_OVERLAY);
+		gtk_layer_set_anchor(gobj(), GTK_LAYER_SHELL_EDGE_TOP, true);
+		gtk_layer_set_exclusive_zone(gobj(), -1);
+		gtk_layer_set_keyboard_interactivity(gobj(), false);
+
 		m_image.set_from_resource("/wlosd/images/volume_muted.png");
-		add(m_image);
+
+		set_opacity(0.4);
+		set_visible(false);
+		set_can_focus(false);
+		set_resizable(false);
+		set_app_paintable(true);
+		set_type_hint(Gdk::WINDOW_TYPE_HINT_NOTIFICATION);
+		set_title("wlosd");
+
+		m_box.set_visible(true);
+
+		add(m_box);
+
+		m_box.add(m_image);
+
 		show_all();
 	}
 
+	void draw(const ::Cairo::RefPtr< ::Cairo::Context> &cr) {
+	    printf("Hello");
+	}
+	
+
     protected:
+	Gtk::Box m_box;
 	Gtk::Image m_image;
 };
 
 int main(int argc, char *argv[])
 {
 	auto app = Gtk::Application::create("org.hello", Gio::APPLICATION_FLAGS_NONE);
-	Daemon d;
+	OsdWindow d;
 
 	app->run(d);
 
